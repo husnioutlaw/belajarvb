5709f9083a5af266c78348b3c918ea02533ba2a3
HusniOutlaw <husnioutlaw@gmail.com>
2016-10-19 17:41:32 +0700

    ubah +2

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -2,7 +2,7 @@
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         For i As Integer = 0 To Val(TextBox2.Text) - 1
-            ListBox1.Items.Add(TextBox1.Text & i + 1)
+            ListBox1.Items.Add(TextBox1.Text & i + 2)
         Next
     End Sub
 End Class
