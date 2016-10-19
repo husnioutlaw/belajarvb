0dc361e40be16449bd4aec6ed9d5d01c9b7d7ed2
HusniOutlaw <husnioutlaw@gmail.com>
2016-10-19 17:21:21 +0700

    udah di benerin

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        For i As Integer = 0 To Val(TextBox2.Text) - 1
+            ListBox1.Items.Add(TextBox1.Text & i + 1)
+        Next
+    End Sub
 End Class
